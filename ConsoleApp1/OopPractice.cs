2f7d1464f8918d215de1604405d7fe55f5d77be9
Egor Svetlichny <fkhor@inbox.ru>
2024-09-04 16:53:20 +0300

    add OOP c# practice

---
+++ ConsoleApp1/OopPractice.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SharpPractice
+{
+    public class Student
+    {
+        public string Name;
+        public int Age;
+        public char Group;
+        public bool HasScgolarship;
+        public float AverageGradePoint;
+
+        public Student(string Name, int age, char group, bool hasScolarship, float averageGradePoint)
+        {
+            this.Name = Name;
+            Age = age;
+            Group = group;
+            HasScgolarship = hasScolarship;
+            AverageGradePoint = averageGradePoint;
+        }
+
+        public void ShowInfo()
+        {
+            string scolarship;
+
+            if (HasScgolarship)
+            {
+                scolarship = "есть";
+            }
+            else
+            {
+                scolarship = "отсутствует";
+            }
+
+            Console.WriteLine($"{Name}, {Age} года, группа \"{Group}\", стипендия {scolarship}, средний бал - {AverageGradePoint};");
+        }
+    }
+
+    class BaseArticle
+    {
+        public string Title;
+
+        public BaseArticle(string title)
+        {
+            Title = title;
+        }
+    }
+
+    class SimpleArticle : BaseArticle
+    {
+        public string Slug;
+
+        public SimpleArticle(string title = "Простая статья", string slug = "simple-article") : base(title)
+        {
+            Slug = slug;
+        }
+    }
+
+    class PrivateArticle : BaseArticle
+    {
+        private string _slug;
+        private DateTime _publishedDate;
+        private int _countOfViews;
+
+        public string Slug { get; private set; }
+        public DateTime PublishedDate { get; private set; }
+        public int CountOfViews
+        {
+            get
+            {
+                return _countOfViews;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Число просмотров не может быть отрицательным", nameof(_countOfViews));
+                }
+                else if (value.GetType() != typeof(int))
+                {
+                    throw new InvalidCastException("Число просмотров должно быть целым числом");
+                }
+                else
+                {
+                    _countOfViews = value;
+                }
+            }
+        }
+
+        public PrivateArticle(string title, string slug, DateTime publishedDate, int countOfViews) : base(title)
+        {
+            Slug = slug;
+            PublishedDate = publishedDate;
+            CountOfViews = countOfViews;
+        }
+
+    }
+
+
+    class Article : BaseArticle
+    {
+        public string Slug;
+        public DateTime PublishedDate;
+        public int CountOfViews;
+
+        public Article(DateTime publishedDate, string title = "", string slug = "slug-1", int countOfViews = 0) : base(title)
+        {
+            Slug = slug;
+            PublishedDate = publishedDate;
+            CountOfViews = countOfViews;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Title}, слаг: {Slug}, опубликована {PublishedDate}, число просмотров: {CountOfViews};");
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -184,15 +184,51 @@ namespace CSLRight
 
 
             //Коллекции в c#
-            CollectionPractice collectionPractice = new CollectionPractice();
-            collectionPractice.WorkArray();
-            collectionPractice.WorkList();
-            collectionPractice.WorkQueue();
-            collectionPractice.WorkStack();
-            collectionPractice.WorkDictionary();
+            //CollectionPractice collectionPractice = new CollectionPractice();
+            //collectionPractice.WorkArray();
+            //collectionPractice.WorkList();
+            //collectionPractice.WorkQueue();
+            //collectionPractice.WorkStack();
+            //collectionPractice.WorkDictionary();
+
+            Student[] stedentsArr =
+            {
+                new Student("Пётр Иванов", 22, 'B', true, 4.7f),
+                new Student("Вася Зайцев", 18, 'A', false, 3.24f),
+                new Student("Дима Клюев", 20, 'B', false, 3.45f),
+                new Student("Андрей Марск", 21, 'A', false, 3.33f),
+                new Student("Ольга Коршун", 20, 'A', true, 4.84f),
+            };
+
+            foreach (var student in stedentsArr)
+                student.ShowInfo();
+            Console.WriteLine();
+
+            Article article1 = new Article(DateTime.Now);
+            Article article2 = new Article(DateTime.Now, "Влияние IT-индустрии на жизнь подростков", "vliyanie-it", 243_256);
+
+            Dictionary<int, Article> articleDict = new Dictionary<int, Article>() { { 1, article1 }, { 2, article2 } };
+            foreach (var articleKey in articleDict.Keys)
+                articleDict[articleKey].ShowInfo();
+
+            Console.WriteLine();
+            Type type = typeof(Article);
+            Console.WriteLine(type);
+            // Вывод родительского класса для класса Article
+            Console.WriteLine(type.BaseType);
+            // Доказательство того, что все классы неявно наследуются от класса Object
+            Console.WriteLine(type.IsSubclassOf(typeof(Object)));
+
+            // Вывод всех классов наследников класса BaseArticle
+            Type[] types = typeof(BaseArticle).Assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(BaseArticle))).ToArray();
+            foreach (var item in types)
+                Console.WriteLine(item);
+
+            // При отрицательных значениях _countOfViews падает ошибка из-за написанной в сеттере валидации
+            PrivateArticle privateArticle = new PrivateArticle("Старлинк и будущее", "starlink-i-budushee", DateTime.Now, 100);
+            Console.WriteLine($"Аттрибут \"{nameof(privateArticle.CountOfViews)}\" объекта \"{nameof(privateArticle)}\" равен {privateArticle.CountOfViews}");
 
             Console.ReadKey();
-
         }
     }
 }
