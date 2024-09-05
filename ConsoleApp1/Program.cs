8bb7b3778cc5f13841407227f9bca3e39da7016b
Egor Svetlichny <fkhor@inbox.ru>
2024-09-05 18:16:28 +0300

    Add OOP and work with files practice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SharpPractice;
+using System.Collections;
 using System.Security.Cryptography;
 
 //namespace CSLight
@@ -142,7 +143,7 @@ namespace CSLRight
             return arr;
         }
 
-        static void AlgoSortingRun()
+        public static void AlgoSortingRun()
         {
             //Сортировка пузырьком
             int[] array = bubbleSort([3, 4, 1, 2, 10, 0, 1, 0, 9, 2]);
@@ -161,6 +162,21 @@ namespace CSLRight
             {
                 Console.Write($"{item} ");
             }
+        }
+
+        public static string searchPerson(List<object[]> matrix, int passportCode, string passportData)
+        {
+            string normolizeData = passportData.ToLower().Replace('ё', 'е').Replace(" ", "");
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                string passportNormolized = ((string)matrix[i][1] + (string)matrix[i][2]).ToLower().Replace('ё', 'е').Replace(" ", "");
+                if ((int)matrix[i][0] == passportCode && passportNormolized == normolizeData)
+                {
+                    return (string)matrix[i][3];
+                }
+            }
+            return $"Пользователь с кодом {passportCode} и паспортом {passportData} не найден!";
         }
 
         private static void Main(string[] args)
@@ -227,6 +243,57 @@ namespace CSLRight
             // При отрицательных значениях _countOfViews падает ошибка из-за написанной в сеттере валидации
             PrivateArticle privateArticle = new PrivateArticle("Старлинк и будущее", "starlink-i-budushee", DateTime.Now, 100);
             Console.WriteLine($"Аттрибут \"{nameof(privateArticle.CountOfViews)}\" объекта \"{nameof(privateArticle)}\" равен {privateArticle.CountOfViews}");
+            Console.WriteLine("\n");
+
+
+            // Работа с файлами
+            // Решение задачи (Имеется двумерный массив, каждый массив которого содержит целое число - код, строку - серию паспорта, строку - номер паспорта, строку - Фамилия И.О.
+            // Задание - написать функцию, которая принимает эту матрицу, код и строку с серией и номером паспорта, которая может содержать пробелы и иметь ошибки в регистрах букв, а также
+            // буква 'е' ровняется 'ё'. Необходимо найти и вывести Фамилию И.О. человека по входным данным.)
+            // Данные о людях (исходная матрица) нужно прочитать из файла и вывести в результирующий файл.
+            MyFile inputFilePath = new MyFile(@"C:\Users\fkhor\VSProjects\ConsoleApp1\ConsoleApp1\inputDataForSorting.txt");
+            MyFile outputFilePath = new MyFile(@"C:\Users\fkhor\VSProjects\ConsoleApp1\ConsoleApp1\outputSortedData.txt");
+            if (inputFilePath.CheckFileExisting() && outputFilePath.CheckFileExisting())
+            {
+                string firstLine = File.ReadLines(inputFilePath.FilePath).FirstOrDefault();
+
+                if (string.IsNullOrEmpty(firstLine))
+                {
+                    Console.WriteLine("Исходный файл пустой!");
+                }
+                else
+                {
+                    string[] fileLines = inputFilePath.ReadFile();
+                    List<object[]> peopleData = new List<object[]>();
+
+                    for (int i = 1; i < fileLines.Length; i++)
+                    {
+                        string[] fileLineArray = fileLines[i].Replace(";", "").Replace("\"", "").Split(',');
+                        peopleData.Add(new object[] { Convert.ToInt32(fileLineArray[0]), fileLineArray[1], fileLineArray[2], fileLineArray[3] });
+
+                    }
+                    outputFilePath.WriteToFile(searchPerson(peopleData, 4, "ж а  дёу3 2 3 3аВ Ыа   2 "));
+                    Console.WriteLine($"Запись из файла по пути:\n{inputFilePath.FilePath}\nв файл по пути:\n{outputFilePath.FilePath}\nпрошла успешно!\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Один из файлов не был найден, проверьте правильность путей!");
+            }
+
+
+            // Работа с концепциями ООП в C#
+            SortingObject sortingObject1 = new SortingObject(10, [1, 3, 5, 1, 0, 7, -2, -3, 4, 4]);
+            SortingObject sortingObject2 = new SortingObject(10, listInt: new List<int> { 1, 3, 5, 1, 0, 7, -2, -3, 4, 4 });
+            SortingObject sortingObject3 = new SortingObject(10, [1, 3, 5, 1, 0, 7, -2, -3, 4, 4], new List<int> { 1, 3, 5, 1, 0, 7, -2, -3, 4, 4 });
+            SortingObject sortingObject4 = new SortingObject();
+
+            foreach (var item in sortingObject2.ListInt)
+                Console.Write($"{item}, ");
+            Console.WriteLine();
+            foreach (var item in sortingObject2.QuickSortObject(sortingObject2.ListInt))
+                Console.Write($"{item}, ");
+            Console.WriteLine();
 
             Console.ReadKey();
         }
---
+++ ConsoleApp1/WorkWithFilesPractice.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpPractice
+{
+    public class MyFile
+    {
+        public string FilePath { get; private set; }
+
+        public MyFile(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public bool CheckFileExisting()
+        {
+            return File.Exists(FilePath);
+        }
+
+        public string[] ReadFile()
+        {
+            string[] fileLines;
+            try
+            {
+                fileLines = File.ReadAllLines(FilePath);
+            }
+            catch (Exception ex)
+            {
+                fileLines = [];
+                Console.WriteLine($"Ошибка чтения файла: {ex.Message}");
+            }
+            return fileLines;
+        }
+
+        public void WriteToFile(string output)
+        {
+            try
+            {
+                
+                using (StreamWriter writer = new StreamWriter(FilePath))
+                {
+                    //foreach (string line in fileLines)
+                    //{
+                    //    writer.WriteLine(line);
+                    //}
+
+                    writer.WriteLine(output);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка записи данных в файл: {ex.Message}");
+            }
+        }
+
+    }
+
+    public class SortingObject
+    {
+        private int[] _arrayInt;
+        private List<int> _listInt;
+        private int _objectLength;
+
+        public int[] ArrayInt { get; private set; }
+        public List<int> ListInt { get; private set; }
+        public int ObjectLength
+        {
+            get
+            {
+                return _objectLength;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Длина не может быть отрицательным числом", nameof(_objectLength));
+                }
+                else
+                {
+                    _objectLength = value;
+                }
+            }
+        }
+
+        public SortingObject(int length = 0, int[] arrayInt = null, List<int> listInt = null)
+        {
+            ObjectLength = length;
+            ArrayInt = arrayInt ?? new int[ObjectLength];
+            ListInt = listInt ?? new List<int>(ObjectLength);
+        }
+
+        public int[] QuickSortObject(int[] arr)
+        {
+            if (arr.Length < 2)
+            {
+                return arr;
+            }
+            else
+            {
+                int pivot = arr[0];
+                int[] less = arr[1..].Where(item => item.CompareTo(pivot) <= 0).ToArray();
+                int[] greater = arr[1..].Where(item => item.CompareTo(pivot) > 0).ToArray();
+
+                return QuickSortObject(less).Concat(new int[] { pivot }).Concat(QuickSortObject(greater)).ToArray();
+            }
+        }
+
+        public List<int> QuickSortObject(List<int> list)
+        {
+            if (list.Count < 2)
+            {
+                return list;
+            }
+            else
+            {
+                int pivot = list[0];
+                List<int> less = list.GetRange(1, list.Count - 1).Where(item => item <= pivot).ToList();
+                List<int> greater = list.GetRange(1, list.Count - 1).Where(item => item > pivot).ToList();
+
+                return QuickSortObject(less).Concat(new List<int>() { pivot }).Concat(QuickSortObject(greater)).ToList();
+            }
+        }
+
+    }
+}
