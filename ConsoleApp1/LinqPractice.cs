c08e15797dc708bb5c446450cd78ab3870fd4be5
Egor Svetlichny <fkhor@inbox.ru>
2024-09-07 18:40:38 +0300

    add new Linq practice with collections

--- ConsoleApp1/LinqPractice.cs
+++ ConsoleApp1/LinqPractice.cs
@@ -77,14 +77,39 @@ namespace SharpPractice
 
             //Linq в сочетании с примитивными типами
             //Фильтрация массива чисел с плавающей точкой
+            float[] array = new float[] { 1.11f, 1.1f, 5.56f, 0.1f, 0f, 0.01f, 0.11f, 0.0f, -2.0f, 4.06f, 5.5f, 7.0f, 9.342f, 1.1f };
+            float[] positiveSortedArr = array.Where(x => x >= 0).OrderBy(x => x).ToArray();
+            foreach (var number in positiveSortedArr)
+                Console.Write($"{number} ");
+            Console.WriteLine();
+
+            //Сортировка и фильтрация в словаре по значениям и ключам
+            Dictionary<int, string> dict = new Dictionary<int, string>() { { 123, "Dad" }, { -1, "beer" }, { 77, "dog" }, { 78, "cat" }, { 0, "dog" }, { -543, "duck" } };
+            Dictionary<int, string> filtredKeysDict = dict.Where(kvp => kvp.Key > 0 && kvp.Value.ToLower().StartsWith('d')).OrderBy(kvp => kvp.Key).ToDictionary();
+            foreach (int key in filtredKeysDict.Keys)
+                Console.Write($"filtredKeysDict[{key}] = {filtredKeysDict[key]}; ");
+            Console.WriteLine();
+
+            //Создание нового объекта на основе другого
+            var filtredPlayersList = playersList.Where(player => player.Level > 12).Select(player => new { Name = player.Name, Level = player.Level, Birthdate = DateTime.Now, Category = 'A' }).ToList();
+            foreach (var player in filtredPlayersList)
+                Console.WriteLine($"Name = {player.Name}, Level = {player.Level}, Birthdate = {player.Birthdate}, Category = {player.Category};");
+            Console.WriteLine();
+
+            //Объединение коллекций при помощи методов расширения
+            List<Player> unitedPlayers = playersOnline.Union(playersList.Where(player => player.Level > 10 && player.MaxScore < 12_000).OrderByDescending(player => player.WinRate)).ToList();
+            OutputPlayersInLine(unitedPlayers);
+
+            //Взять или пропустить определённое количество элементов, пока выполняется условие (в данном случае вывод всех игроков с иментами начинающимися на букву 'Д')
+            OutputPlayersInLine(playersList.OrderBy(player => player.Name).SkipWhile(player => !player.Name.StartsWith('Д')).TakeWhile(player => player.Name.StartsWith('Д')).ToList());
         }
 
         //Функция вывода списка игроков с отступом строки
         private static void OutputPlayersInLine(List<Player> players)
         {
             foreach (var player in players)
-                Console.Write($"{player.Name}. ");
-            Console.WriteLine();
+                Console.Write($"{player.Name}; ");
+            Console.WriteLine("\n");
         }
     }
 }
